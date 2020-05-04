688c1fb28a723fa4311b43d50b0c65c037243906
saikrishna1997 <31088933+saikrishna1997@users.noreply.github.com>
2020-05-04 18:06:28 +0530

    Add files via upload

---
+++ abc/src/main/java/test/abc/Add.java
@@ -0,0 +1,14 @@
+package test.abc;
+
+public class Add {
+
+	public void add(int x,int y){
+		int z;
+		z=x+y;
+		System.out.println("Addition Value :"+z);
+		
+		
+	}
+	
+	
+}
---
+++ abc/src/main/java/test/main/main.java
@@ -0,0 +1,16 @@
+package test.main;
+
+import test.abc.Add;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10,y=20;
+		
+		Add obj =new Add();
+		obj.add(x, y);
+	}
+
+}
